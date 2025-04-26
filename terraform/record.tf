bf6fe4319c6ca1404afd1dc375a55a5fb23dbee5
Nidhi Gupta <nidhigupta@Nidhis-MacBook-Pro.local>
2025-04-26 16:48:07 -0400

    commednted route53 record

--- terraform/record.tf
+++ terraform/record.tf
@@ -1,4 +1,4 @@
-resource "aws_route53_record" "eks_record" {
+/*resource "aws_route53_record" "eks_record" {
   zone_id = data.aws_route53_zone.zone.zone_id
   name    = "hello.${var.environment}.${var.domain_name}"
   type    = "A"
@@ -8,7 +8,7 @@ resource "aws_route53_record" "eks_record" {
     evaluate_target_health = true
   }
 }
-
+*/
 data "aws_lb" "lb" {
   name = "${var.environment}-microservices-alb"  
 }
