7c154b801df8d9242a3377fb750b54c0f352df4f
Nidhi Gupta <nidhigupta@Nidhis-MacBook-Pro.local>
2025-04-26 11:25:27 -0400

    test

--- terraform/data.tf
+++ terraform/data.tf
@@ -5,3 +5,9 @@ data "aws_eks_cluster" "cluster" {
 data "aws_eks_cluster_auth" "cluster" {
   name = data.aws_eks_cluster.cluster.name
 }
+
+data "aws_acm_certificate" "acm" {
+  domain = var.domain_name   # Use wildcard or specific domain
+  types  = ["AMAZON_ISSUED"]    # Filter for Amazon-issued certificates
+  most_recent = true            # Use the most recently issued certificate
+}
